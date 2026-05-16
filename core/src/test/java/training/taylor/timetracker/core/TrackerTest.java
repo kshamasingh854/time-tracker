0b35175205266c29be8041635a69e60cfffabb5c
kshamasingh854 <kshamasingh854@gmail.com>
2026-05-16 13:27:22 +0530

    Update TrackerTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -1,26 +1,28 @@
 package training.taylor.timetracker.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import training.taylor.timetracker.core.dao.TimeEntry;
 
 /**
- * Created by Jason on 6/19/2015.
+ * Fixed Test for Java 17 & Spring 5.3.x
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TrackerCoreConfig.class)
 public class TrackerTest {
+
     @Autowired
     private Tracker tracker;
 
     @Test
     public void testMe() {
-        assertNotNull(tracker);
+        // Ensure tracker bean is created
+        assertNotNull("Tracker bean should not be null", tracker);
     }
 
     @Test
@@ -29,7 +31,9 @@ public class TrackerTest {
         entry.setDescription("Entry Test");
         entry.setRate(80.0f);
         entry.setTime(3);
+
         tracker.add(entry);
-        assertTrue(tracker.size() > 0);
+
+        assertTrue("Tracker size must be greater than 0 after adding", tracker.size() > 0);
     }
 }
